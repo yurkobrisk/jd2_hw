ec6ecbed81c802fd302c2db8453ec553295efcf0
Yury Korziuk <yurko@tut.by>
2021-01-05 15:55:48 +0300

    Commit task8. Servlet module 2, topic 2. Added simple servlet

---
+++ servlet_topic2/task8/src/main/java/jd2_hw/servlet_topic2/task8/Task8Servlet.java
@@ -0,0 +1,27 @@
+package jd2_hw.servlet_topic2.task8;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Task8Servlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head><title>First Servlet</title></head>");
+        out.println("<body><h1>This is Task 8 Servlet for topic 2: servlet</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
